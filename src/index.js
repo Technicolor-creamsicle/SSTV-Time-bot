d034445e768f954f6f5f048b1f7965a8b5f7b5d2
alex <alex@2a03.party>
2020-06-19 07:09:09 -0500

    rewriting stuff

--- src/index.js
+++ src/index.js
@@ -13,42 +13,27 @@ const config = require("./config.json");
 // This is making clients
 const client = new Discord.Client();
 
-const channel = client.channels.cache.get(config.channel);
-
 
 
 //on ready join vc channel and start playing a jingle
 client.on("ready", () => {
+  const channel = client.channels.cache.get(config.channel);
+
+  console.log(channel)
+
   //set activity to "LISTENING TO all the time; all the time"
   client.user.setActivity("all the time; all the time", {type:"LISTENING"});
   console.info("bot is online!");
-  const firstjoin = client.channels.cache.get(config.channel);
+  
   //Bot connecting to VC
-  // eslint-disable-next-line no-unused-vars
-  firstjoin.join().then(connection => {
-      //YeS.(
-      console.log("Successfully connected.");
-      connection.play('./assets/chime.opus');
-    }).catch(e => {
-      //Oh no, bad...
-      console.error(e);
-    });
+  const stream = channel.join();
+  //playing join chime
+  stream.play();
+
 });
 
 
-client.on("disconnect", () => {
-  const rejoin = channel
-  console.warn("Bot has been disconnected... Attempting reconnect!");
-  //Rejoins the bot to the vc
-  // eslint-disable-next-line no-unused-vars
-  rejoin.join().then(connection => {
-    //YeS.
-    console.log("Successfully connected.");
-  }).catch(e => {
-    //Oh no, bad...
-    console.error(e);
-  });
-});
+
 
 //This logs some errors to console thus not causing it to crash as node is "quirky"
 process.on("unhandledRejection", console.error);
