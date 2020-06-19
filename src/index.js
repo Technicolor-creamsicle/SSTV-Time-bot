d2be02ea8753dd9dce2e2f5486613f09ff82d219
alex <alex@2a03.party>
2020-06-19 06:33:58 -0500

    push

--- src/index.js
+++ src/index.js
@@ -5,16 +5,16 @@
 
 //importing modules
 const Discord = require("discord.js");
-const fs = require("fs");
+//const fs = require("fs");
 
 //importing files
-import config from "./config.json";
+const config = require("./config.json");
 
 // This is making clients
 const client = new Discord.Client();
 
 //set activity to "LISTENING TO all the time; all the time"
-client.user.setActivity("all the time; all the time",{type:"LISTENING"});
+client.user.setActivity("all the time; all the time", {type:"LISTENING"});
 
 //on ready join vc channel and start playing a jingle
 client.on("ready", () => {
