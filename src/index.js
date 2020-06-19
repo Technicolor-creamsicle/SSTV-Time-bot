548e1c4585ab81a96c3cb6a75835bf1e06fa22f1
alex <alex@2a03.party>
2020-06-19 02:40:00 -0500

    importing libs making client

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
+#!/usr/bin/env node
 //A Janky Discord.js bot that screams SSTV and in existensial pain
+//Quirky ESlint stuff
+
+
+//importing modules
 import Discord from "discord.js";
 import fs from "fs";
 
+//importing files
+import config from "./config.json";
+// This is making clients
+cosnt client = new Discord.Client();
